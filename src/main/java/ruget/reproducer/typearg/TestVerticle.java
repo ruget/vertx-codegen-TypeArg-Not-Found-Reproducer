f4c1cc51257d71b8c9ac43c28268af50713af575
Joan Ruget <joan.ruget@gmail.com>
2018-12-17 18:43:13 +0100

    Try with rxjava1

--- src/main/java/ruget/reproducer/typearg/TestVerticle.java
+++ src/main/java/ruget/reproducer/typearg/TestVerticle.java
@@ -1,6 +1,6 @@
 package ruget.reproducer.typearg;
 
-import io.vertx.reactivex.core.AbstractVerticle;
+import io.vertx.rxjava.core.AbstractVerticle;
 
 public class TestVerticle extends AbstractVerticle {
 
